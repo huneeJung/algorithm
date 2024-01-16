31c14aa0afaff6cfefcee02ed1b505e8004272d9
JungCheolHoon <cjswovkdnj12@naver.com>
2024-01-16 22:35:25 +0900

    물병 : 1052

---
+++ src/물병/NO_1052.java
@@ -0,0 +1,35 @@
+package 물병;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class NO_1052 {
+    public static void main(String[] args) throws IOException {
+        try (
+                var br = new BufferedReader(new InputStreamReader(System.in));
+                var bw = new BufferedWriter(new OutputStreamWriter(System.out))
+        ) {
+            var st = new StringTokenizer(br.readLine(), " ");
+            var water = Integer.parseInt(st.nextToken());
+            var bitCnt = Integer.bitCount(water);
+            var maxNum = water;
+            var bottle = Integer.parseInt(st.nextToken());
+            int check = 1 << 24;
+            var answer = 0;
+            if (bottle < bitCnt) {
+                while (check != 0) {
+                    if ((check & water) != 0) {
+                        if (bottle == 1) break;
+                        if (bottle > 1) {
+                            maxNum -= check;
+                            bottle--;
+                        }
+                    }
+                    check = check >> 1;
+                }
+                answer = (check << 1) - maxNum;
+            }
+            bw.write(String.valueOf(answer));
+        }
+    }
+}
