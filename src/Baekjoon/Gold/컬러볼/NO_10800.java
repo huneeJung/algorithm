528666743cd45caa65d413f7fd1314f1a7dc552c
JungCheolHoon <cjswovkdnj12@naver.com>
2024-03-20 21:26:07 +0900

    컬러볼 : NO_10800

--- src/Baekjoon/Gold/컬러볼/NO_10800.java
+++ src/Baekjoon/Gold/컬러볼/NO_10800.java
@@ -3,6 +3,18 @@ package Baekjoon.Gold.컬러볼;
 import java.io.*;
 import java.util.*;
 
+class Ball {
+    public int no;
+    public int color;
+    public int size;
+
+    public Ball(int no, int color, int size) {
+        this.no = no;
+        this.color = color;
+        this.size = size;
+    }
+}
+
 public class NO_10800 {
     public static void main(String[] args) throws IOException {
         try (
@@ -10,40 +22,29 @@ public class NO_10800 {
                 var bw = new BufferedWriter(new OutputStreamWriter(System.out))
         ) {
             var ballCnt = Integer.parseInt(br.readLine());
-            List<int[]> ballList = new ArrayList<>();
+            List<Ball> ballList = new ArrayList<>();
             Map<Integer, Integer> sum = new HashMap<>();
             for (int i = 0; i < ballCnt; i++) {
                 var st = new StringTokenizer(br.readLine(), " ");
                 var no = i;
                 var color = Integer.parseInt(st.nextToken());
                 var size = Integer.parseInt(st.nextToken());
-                ballList.add(new int[]{no, color, size});
+                ballList.add(new Ball(no, color, size));
                 if (sum.containsKey(color)) continue;
                 sum.put(color, 0);
             }
-            ballList.sort((a, b) -> a[2] - b[2]);
+            ballList.sort((a, b) -> a.size - b.size);
             int[] answer = new int[ballCnt];
             int total = 0;
-            int beforeSize = 0;
-            int beforeColor = -1;
-            int same = 0;
-            for (int[] ball : ballList) {
-                var no = ball[0];
-                var color = ball[1];
-                var size = ball[2];
-                answer[no] = total - sum.get(color);
-                if (beforeSize == size) {
-                    same++;
-                    if (beforeColor != color) {
-                        answer[no] -= size * same;
-                    }
-                } else {
-                    same = 0;
+            int index = 0;
+            for (Ball ball : ballList) {
+                var before = ballList.get(index);
+                while (before.size < ball.size) {
+                    total += before.size;
+                    sum.merge(before.color, before.size, Integer::sum);
+                    before = ballList.get(++index);
                 }
-                total += size;
-                sum.merge(color, size, Integer::sum);
-                beforeSize = size;
-                beforeColor = color;
+                answer[ball.no] = total - sum.get(ball.color);
             }
             for (int num : answer) {
                 bw.write(num + "\n");
